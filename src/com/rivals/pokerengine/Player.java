d7e348fe89b6edcfc044bbcad45c92347e070110
ultimateck <charaka90@gmail.com>
2014-07-22 14:43:47 +0530

    Player Class Added

---
+++ src/com/rivals/pokerengine/Player.java
@@ -0,0 +1,52 @@
+package com.rivals.pokerengine;
+
+import java.util.*;
+
+public class Player {
+	private int id;
+	private String nickName;
+	private List<Card> hand;
+	
+	public Player(){
+		hand = new ArrayList<Card>();
+	}
+	
+	public Player(String name, int id){
+		this();
+		this.nickName = name;
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public List<Card> getHand() {
+		return hand;
+	}
+
+	public void setHand(List<Card> hand) {
+		this.hand = hand;
+	}
+	
+	public void addCardToHand(Card card){
+		hand.add(card);
+	}
+	
+	public Card getCardAt(int index) throws IndexOutOfBoundsException{
+		return hand.get(index);
+	}
+
+}
