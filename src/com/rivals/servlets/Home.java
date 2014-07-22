9d00207db4b543efb7e56b5246954bd744314fa7
Charaka <Charaka@Sargas>
2014-07-22 21:52:12 +0530

    Fixed for first run

--- src/com/rivals/servlets/Home.java
+++ src/com/rivals/servlets/Home.java
@@ -28,7 +28,7 @@ public class Home extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		show(request, response);
 	}
 
 	/**
